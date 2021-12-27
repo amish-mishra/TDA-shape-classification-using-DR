ed7af793eab0547121f8b3c30aa574518fa56466
amish-mishra <mishra.amish@gmail.com>
2021-12-26 21:44:06 -0500

    updated dunn-index to include graphing vs noise and rips and alpha

--- Delaunay-Rips_Paper/code/Dunn_index_measure.py
+++ Delaunay-Rips_Paper/code/Dunn_index_measure.py
@@ -35,146 +35,92 @@ def inter_cluster_min_dist(cluster1, cluster2): # returns the distance between t
     return min_dist
 
 
-num_per_cluster = 30
+def dunn_index(all_clusters):
+    min_inter_dist = None
+    max_intra_dist = -1
+    for i, C_i in enumerate(all_clusters):
+        for j, C_j in enumerate(all_clusters):
+            if i != j:  # found different clusters
+                inter_dist = inter_cluster_min_dist(C_i, C_j)
+                if min_inter_dist is None:
+                    min_inter_dist = inter_dist
+                if inter_dist < min_inter_dist:
+                    min_inter_dist = inter_dist
+        intra_dist = intra_cluster_max_dist(C_i)
+        if intra_dist > max_intra_dist:
+            max_intra_dist = intra_dist
+    dunn_index = min_inter_dist/max_intra_dist
+    return dunn_index
+
+
+start_noise = 0.001
+max_noise = 0.5
+noise_inc = .05
+noise_arr = np.array(start_noise)
+num_per_cluster = 20
 num_points = 100
-noise = 0.01
 hom_class = 2
-PD_dataset1 = [None]*num_per_cluster
-PD_dataset2 = [None]*num_per_cluster
+noise_arr = np.empty(0)
+dunn_index_arr_DR = np.empty(0)
+dunn_index_arr_R = np.empty(0)
+dunn_index_arr_A = np.empty(0)
+del_rips_color = 'b'
+rips_color = 'r'
+alpha_color = 'g'
 
-for i in range(num_per_cluster):
-    # Delaunay-Rips
-    filtration = DR.build_filtration(tadasets.dsphere(n=num_points, d=2, r=1, noise=noise), hom_dim=hom_class)
-    PD_dataset1[i] = cm.phat_diagrams(filtration, show_inf=True, verbose=False)[hom_class]
-    filtration = DR.build_filtration(tadasets.torus(n=num_points, c=2, a=1, noise=noise), hom_dim=hom_class)
-    PD_dataset2[i] = cm.phat_diagrams(filtration, show_inf=True, verbose=False)[hom_class]
+for curr_noise in np.arange(start_noise + noise_inc, max_noise, noise_inc):
+    rounded_curr_noise = round(curr_noise, 3)
+    print(rounded_curr_noise)
+    PD_dataset1 = [[None for i in range(num_per_cluster)] for j in range(3)]
+    PD_dataset2 = [[None for i in range(num_per_cluster)] for j in range(3)]
+    for i in range(num_per_cluster):
+        data1 = tadasets.dsphere(n=num_points, d=2, r=1, noise=curr_noise)
+        data2 = tadasets.torus(n=num_points, c=2, a=1, noise=curr_noise)
 
-all_clusters = [PD_dataset1, PD_dataset2]
-min_inter_dist = None
-max_intra_dist = -1
-for i, C_i in enumerate(all_clusters):
-    for j, C_j in enumerate(all_clusters):
-        if i != j:  # found different clusters
-            inter_dist = inter_cluster_min_dist(C_i, C_j)
-            if min_inter_dist is None:
-                min_inter_dist = inter_dist
-            if inter_dist < min_inter_dist:
-                min_inter_dist = inter_dist
-    intra_dist = intra_cluster_max_dist(C_i)
-    if intra_dist > max_intra_dist:
-        max_intra_dist = intra_dist
+        # Delaunay-Rips
+        filtration = DR.build_filtration(data1, hom_dim=hom_class)
+        PD_dataset1[0][i] = cm.phat_diagrams(filtration, show_inf=True, verbose=False)[hom_class]
+        filtration = DR.build_filtration(data2, hom_dim=hom_class)
+        PD_dataset2[0][i] = cm.phat_diagrams(filtration, show_inf=True, verbose=False)[hom_class]
 
-dunn_index = min_inter_dist/max_intra_dist
-print(dunn_index)
+        # Rips
+        PD_dataset1[1][i] = ripser(data1, maxdim=hom_class)['dgms'][hom_class]
+        PD_dataset2[1][i] = ripser(data2, maxdim=hom_class)['dgms'][hom_class]
 
+        # Alpha
+        alpha = cm.Alpha(verbose=False)
+        alpha_filtration = alpha.build(2*data1)
+        PD_dataset1[2][i] = alpha.diagrams(alpha_filtration)[hom_class]
+        alpha = cm.Alpha(verbose=False)
+        alpha_filtration = alpha.build(2*data2)
+        PD_dataset2[2][i] = alpha.diagrams(alpha_filtration)[hom_class]
 
+    all_clusters_DR = [PD_dataset1[0], PD_dataset2[0]]
+    all_clusters_R = [PD_dataset1[1], PD_dataset2[1]]
+    all_clusters_A = [PD_dataset1[2], PD_dataset2[2]]
 
+    # Calculate Dunn-index
+    dunn_index_DR = dunn_index(all_clusters_DR)
+    dunn_index_R = dunn_index(all_clusters_R)
+    dunn_index_A = dunn_index(all_clusters_A)
 
+    noise_arr = np.append(noise_arr, rounded_curr_noise)
+    dunn_index_arr_DR = np.append(dunn_index_arr_DR, dunn_index_DR)
+    dunn_index_arr_R = np.append(dunn_index_arr_R, dunn_index_R)
+    dunn_index_arr_A = np.append(dunn_index_arr_A, dunn_index_A)
 
+plt.plot(noise_arr, dunn_index_arr_DR, color=del_rips_color, label='Del-Rips')
+plt.plot(noise_arr, dunn_index_arr_R, color=rips_color, label='Rips')
+plt.plot(noise_arr, dunn_index_arr_A, color=alpha_color, label='Alpha')
 
+# Naming the x-axis, y-axis and the whole graph
+plt.xlabel("Noise", fontsize=16)
+plt.xlim([0, max_noise])
+plt.ylabel("Dunn Index", fontsize=16)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
 
+# Adding legend, which helps us recognize the curve according to it's color
+plt.legend(fontsize=14)
 
-
-
-
-# def perturb(data, noise):
-#     perturb_vects = np.random.randn(len(data),len(data[0]))
-#     mags = np.linalg.norm(perturb_vects, axis=1)
-#     for i, mag in enumerate(mags):
-#         perturb_vects[i] /= mag*(1/noise) # check that this is actually appying to the whole row. Also, perturb by at MOST noise, not exactly noise
-#     perturbed_data = data + perturb_vects
-#     return(perturbed_data)
-
-
-# start_noise = 0.0001
-# max_noise = 2
-# noise_inc = .1
-# pts = 100
-# dim = 2
-# hom_class = 1
-# trials = 5
-# del_rips_color = 'b'
-# rips_color = 'r'
-# alpha_color = 'g'
-
-# data = tadasets.dsphere(n=pts, d=dim, r=1, noise=start_noise)
-# # plt.scatter(data[:,0], data[:,1])
-# # plt.show()
-
-# # Delaunay-Rips on original data
-# filtration = DR.build_filtration(data, dim)
-# original_PD_DR  = cm.phat_diagrams(filtration, show_inf=True, verbose=False)[hom_class]
-
-# # Rips on original data
-# original_PD_R = ripser(data, maxdim=dim)['dgms'][hom_class]
-
-# # Alpha on original data
-# alpha = cm.Alpha(verbose=False)
-# alpha_filtration = alpha.build(2*data)
-# original_PD_A = alpha.diagrams(alpha_filtration)[hom_class]
-# # plot_diagrams(original_PD_DR, show=True)
-# # plot_diagrams(original_PD_R, show=True)
-# # plot_diagrams(original_PD_A, show=True)
-
-# noise_arr = np.array(start_noise)
-# bott_dist_arr_DR = np.array(0)
-# bott_dist_arr_R = np.array(0)
-# bott_dist_arr_A = np.array(0)
-# for curr_noise in np.arange(start_noise + noise_inc, max_noise, noise_inc):
-#     rounded_curr_noise = round(curr_noise, 3)
-#     DR_trial_array = [None]*trials
-#     R_trial_array = [None]*trials
-#     A_trial_array = [None]*trials
-#     for t in range(trials):
-#         perturbed_data = perturb(data, rounded_curr_noise)
-#         # Delaunay-Rips
-#         filtration = DR.build_filtration(perturbed_data, dim)
-#         dgms_dr = cm.phat_diagrams(filtration, show_inf=True, verbose=False)[hom_class]
-#         bott_dist = bottleneck(original_PD_DR, dgms_dr)
-#         DR_trial_array[t] = bott_dist
-        
-
-#         # Rips
-#         dgms_rips = ripser(perturbed_data, maxdim=dim)['dgms'][hom_class]
-#         bott_dist = bottleneck(original_PD_R, dgms_rips)
-#         R_trial_array[t] = bott_dist
-
-#         # Alpha
-#         alpha = cm.Alpha(verbose=False)
-#         alpha_filtration = alpha.build(2*perturbed_data)
-#         dgms_alpha = alpha.diagrams(alpha_filtration)[hom_class]
-#         bott_dist = bottleneck(original_PD_A, dgms_alpha)
-#         A_trial_array[t] = bott_dist
-
-#     bott_dist_arr_DR = np.append(bott_dist_arr_DR, np.median(DR_trial_array))
-#     bott_dist_arr_R = np.append(bott_dist_arr_R, np.median(R_trial_array))
-#     bott_dist_arr_A = np.append(bott_dist_arr_A, np.median(A_trial_array))
-#     plt.boxplot(DR_trial_array, showfliers=False, positions=[rounded_curr_noise], 
-#                 widths=noise_inc/5, boxprops=dict(color=del_rips_color), capprops=dict(color=del_rips_color),
-#                 whiskerprops=dict(color=del_rips_color), medianprops=dict(color=del_rips_color))
-#     plt.boxplot(R_trial_array, showfliers=False, positions=[rounded_curr_noise], 
-#                 widths=noise_inc/5, boxprops=dict(color=rips_color), capprops=dict(color=rips_color),
-#                 whiskerprops=dict(color=rips_color), medianprops=dict(color=rips_color))
-#     plt.boxplot(A_trial_array, showfliers=False, positions=[rounded_curr_noise], 
-#                 widths=noise_inc/5, boxprops=dict(color=alpha_color), capprops=dict(color=alpha_color),
-#                 whiskerprops=dict(color=alpha_color), medianprops=dict(color=alpha_color))
-
-#     noise_arr = np.append(noise_arr, rounded_curr_noise)
-
-# # Plotting both the curves simultaneously
-# # print(bott_dist_arr_A, bott_dist_arr_R, bott_dist_arr_DR)
-# plt.plot(noise_arr, bott_dist_arr_DR, color=del_rips_color, label='Del-Rips')
-# plt.plot(noise_arr, bott_dist_arr_R, color=rips_color, label='Rips')
-# plt.plot(noise_arr, bott_dist_arr_A, color=alpha_color, label='Alpha')
-
-# # Naming the x-axis, y-axis and the whole graph
-# plt.xlabel("Noise", fontsize=16)
-# plt.xlim([0, max_noise])
-# plt.ylabel("Bottleneck Distance", fontsize=16)
-# plt.xticks(fontsize=14)
-# plt.yticks(fontsize=14)
-
-# # Adding legend, which helps us recognize the curve according to it's color
-# plt.legend(fontsize=14)
-
-# plt.show()
+plt.show()
