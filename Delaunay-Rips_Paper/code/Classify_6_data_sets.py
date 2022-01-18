8c314257e535502331938b445505f76789c2f110
amish-mishra <mishra.amish@gmail.com>
2022-01-18 17:11:02 -0500

    added comments from Dr. Motta

--- Delaunay-Rips_Paper/code/Classify_6_data_sets.py
+++ Delaunay-Rips_Paper/code/Classify_6_data_sets.py
@@ -1,7 +1,7 @@
 # Author: Amish Mishra
 # Date: January 18, 2022
 # README: Classify 6 data sets using Del-rips, Rips, and Alpha
-
+# Don't concatenate dgms and then compute persistent images; compute images from H_0 and and H_1 and then concatenate
 
 import numpy as np
 import matplotlib.pyplot as plt
