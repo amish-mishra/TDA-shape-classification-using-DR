8b1a1042f81eed61555561456fd3a3c6fb50cf13
amish-mishra <mishra.amish@gmail.com>
2022-01-26 12:27:23 -0500

    cluster shapes are scaled down by diameter now

--- Delaunay-Rips_Paper/code/Generate_PD_in_txt.py
+++ Delaunay-Rips_Paper/code/Generate_PD_in_txt.py
@@ -34,7 +34,9 @@ def generate_noisy_data(shape, max_noise, pts):
     elif shape.lower() == "random":
         data = np.random.rand(pts, 3)
     elif shape.lower() == "clusters":
-        centers = np.random.rand(3, 3);  
+        centers = np.random.rand(3, 3);
+        dist_matrix = cdist(centers, centers, metric='euclidean')
+        centers = centers/dist_matrix.max() # scale centers to have diam 1 here because it is faster to do it now
         a = np.random.randint(np.floor(.1*pts), np.floor(.45*pts))  # random number of points for first cluster, can be at most 45% of all points, but is at least 10%
         b =  np.random.randint(np.floor(.1*(pts-a)), np.floor(.45*(pts-a))) # random number of pts in 2nd cluster, at least 10% remaining, at most 45%
         c = pts-a-b
@@ -48,16 +50,21 @@ def generate_noisy_data(shape, max_noise, pts):
         cluster_centers = np.concatenate((np.tile(centers_3[0,:], (3, 1)), np.tile(centers_3[1,:], (3, 1)),  np.tile(centers_3[2,:], (3, 1))))
         perturb_centers = max_noise*np.random.rand(9, 3)
         centers_9 = cluster_centers + perturb_centers # create 9 cluster centers; 3 clusters each in 3 large clusters
+        dist_matrix = cdist(centers_9, centers_9, metric='euclidean')
+        centers_9 = centers_9/dist_matrix.max() # scale centers to have diam 1 here because it is faster to do it now
+
         # Create cluster 1 just like in the "clusters" case
         a1 = np.random.randint(np.floor(.1*a), np.floor(.45*a))
         a2 =  np.random.randint(np.floor(.1*(a-a1)), np.floor(.45*(a-a1)))
         a3 = a-a1-a2
         cluster1 = np.concatenate((np.tile(centers_9[0,:], (a1, 1)), np.tile(centers_9[1,:], (a2, 1)),  np.tile(centers_9[2,:], (a3, 1))))
+
         # Create cluster 2 just like in the "clusters" case
         a1 = np.random.randint(np.floor(.1*b), np.floor(.45*b))
         a2 =  np.random.randint(np.floor(.1*(b-a1)), np.floor(.45*(b-a1)))
         a3 = b-a1-a2
         cluster2 = np.concatenate((np.tile(centers_9[3,:], (a1, 1)), np.tile(centers_9[4,:], (a2, 1)),  np.tile(centers_9[5,:], (a3, 1))))
+
         # Create cluster 3 just like in the "clusters" case
         a1 = np.random.randint(np.floor(.1*c), np.floor(.45*c))
         a2 =  np.random.randint(np.floor(.1*(c-a1)), np.floor(.45*(c-a1)))
@@ -65,8 +72,9 @@ def generate_noisy_data(shape, max_noise, pts):
         cluster3 = np.concatenate((np.tile(centers_9[6,:], (a1, 1)), np.tile(centers_9[7,:], (a2, 1)),  np.tile(centers_9[8,:], (a3, 1))))
 
         data = np.concatenate((cluster1, cluster2, cluster3))
-
-    data = data/get_diameter(data)  # scale data down to have diameter 1
+    tic = time.time()
+    data = data/get_diameter(data, shape)  # scale data down to have diameter 1
+    print("Took", time.time()-tic)
     return perturb(data, max_noise)
 
 
@@ -88,22 +96,25 @@ def get_pd(filtration_method, data):
     return dgms
 
 
-def get_diameter(X):
-    print(len(X)) 
-    hull = ConvexHull(X) # Find a convex hull in O(N log N)
-    hullpoints = X[hull.vertices,:] # Extract the points forming the hull
-    # Naive way of finding the best pair in O(H^2) time if H is number of points on hull
-    hdist = cdist(hullpoints, hullpoints, metric='euclidean')
-    return hdist.max()
-    # Get the farthest apart points
-    # bestpair = np.unravel_index(hdist.argmax(), hdist.shape)
-    # print([hullpoints[bestpair[0]], hullpoints[bestpair[1]]])
+def get_diameter(X, shape):
+    if shape.lower() in ["clusters", "clusters_in_clusters"]: # this has already been handled when clusters were generated
+    # ConvexHull will not work for 3 clusters of many repeated points because they only span a plane, not a 3-D space
+        return 1      
+    else:
+        hull = ConvexHull(X) # Find a convex hull in O(N log N)
+        hullpoints = X[hull.vertices,:] # Extract the points forming the hull
+        # Naive way of finding the best pair in O(H^2) time if H is number of points on hull
+        hdist = cdist(hullpoints, hullpoints, metric='euclidean')
+        return hdist.max()
+        # Get the farthest apart points
+        # bestpair = np.unravel_index(hdist.argmax(), hdist.shape)
+        # print([hullpoints[bestpair[0]], hullpoints[bestpair[1]]])
 
 
 # Testing
 shape = 'clusters'
 filt_func = 'Alpha'
-X = generate_noisy_data(shape, 0.01, 500)   # generate data for a shape
+X = generate_noisy_data(shape, 0.1, 500)   # generate data for a shape
 fig = plt.figure()
  # syntax for 3-D projection
 ax = plt.axes(projection ='3d')
