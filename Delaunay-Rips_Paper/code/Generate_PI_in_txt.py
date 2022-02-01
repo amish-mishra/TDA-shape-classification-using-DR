1ef6babc9cfc1a06a48176b9d37ef48ceada51e6
amish-mishra <mishra.amish@gmail.com>
2022-02-01 17:04:46 -0500

    got PIs to display correctly

--- Delaunay-Rips_Paper/code/Generate_PI_in_txt.py
+++ Delaunay-Rips_Paper/code/Generate_PI_in_txt.py
@@ -12,26 +12,25 @@ shape_name = "Circle"
 i = 0
 j = 0
 
-pd = np.loadtxt("C:\\Users\\amish\Documents\\research\\Delaunay-Rips_Paper\\pd_noise_0_05\\\Rips\\Torus\\PD_24_1.txt")
+pd = np.loadtxt("C:\\Users\\amish\Documents\\research\\Delaunay-Rips_Paper\\pd_noise_0_05\\\Rips\\Circle\\PD_24_1.txt")
 
+print(pd)
 
 # Printing a PersistenceImager() object will print its defining attributes
 pimgr = PersistenceImager(pixel_size=0.2, birth_range=(0,1))
 # PersistenceImager() attributes can be adjusted at or after instantiation.
 # Updating attributes of a PersistenceImager() object will automatically update all other dependent attributes.
-pimgr.pixel_size = 0.1
-pimgr.birth_range = (0, 2)
+pimgr.pixel_size = 0.01
+# pimgr.birth_range = (0, 0.2)
 # The `fit()` method can be called on one or more (*,2) numpy arrays to automatically determine the miniumum birth and
 # persistence ranges needed to capture all persistence pairs. The ranges and resolution are automatically adjusted to
 # accomodate the specified pixel size.
-pimgr = PersistenceImager(pixel_size=0.02)
-pdgms = [np.array([[0.5, 0.8], [0.7, 2.2], [2.5, 4.0]]),
-         np.array([[0.1, 0.2], [3.1, 3.3], [1.6, 2.9]]),
-         np.array([[0.2, 1.5], [0.4, 0.6], [0.2, 2.6]])]
 pimgr.fit(pd, skew=True)
 # The `transform()` method can then be called on one or more (*,2) numpy arrays to generate persistence images from diagrams.
 # The option `skew=True` specifies that the diagrams are currently in birth-death coordinates and must first be transformed
 # to birth-persistence coordinates.
+pimgr.weight_params = {'n': 1.0}
+pimgr.kernel_params = {'sigma': [[0.00001, 0.0], [0.0, 0.00001]]}
 pimgs = pimgr.transform(pd, skew=True)
 # The `plot_diagram()` and `plot_image()` methods can be used to visualize persistence diagrams and images
 fig, axs = plt.subplots(1, 3, figsize=(10,5))
@@ -43,8 +42,16 @@ axs[1].set_title("Birth-Persistence\nCoordinates")
 pimgr.plot_diagram(pd, skew=True, ax=axs[1])
 
 axs[2].set_title("Persistence Image")
+
 pimgr.plot_image(pimgs, ax=axs[2])
 print(pimgs)
 
 plt.tight_layout()
 plt.show()
+
+
+''''
+Motta notes:
+- Put all H_k pds in a k_dgm array and run the .fit transformer on them to set the scale of the diagram
+- 
+'''
